8ca5273f2a7eae84c90c5ff6bd656b3425720454
coolaxguy <andrew.l.hoffman29@gmail.com>
2025-04-07 15:45:27 -0500

    first sql box filled

--- OUFitRecDatabase/OUFitRecDatabase.sql
+++ OUFitRecDatabase/OUFitRecDatabase.sql
@@ -1,1 +1,1 @@
-﻿select * from IMSport
+﻿select * from Home
--- OUFitRecWebApp/MainWindow.xaml.cs
+++ OUFitRecWebApp/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Data.Entity;
 
 namespace OUFitRecWebApp
 {
@@ -23,6 +27,7 @@ namespace OUFitRecWebApp
         public MainWindow()
         {
             InitializeComponent();
+            LoadData();
         }
 
         private void HPFE_Click(object sender, RoutedEventArgs e)
@@ -73,5 +78,24 @@ namespace OUFitRecWebApp
             f45.Show();
             this.Close();
         }
+        private void LoadData()
+        {
+            // Retrieve the connection string from App.config
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDatabaseContext"].ConnectionString;
+
+            string query = "SELECT Body FROM Home WHERE ContentID = 1";
+
+            // Connect to the database and retrieve the data
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+
+                // Execute the query and get the result
+                var data = cmd.ExecuteScalar();  // Or ExecuteReader() if fetching multiple rows
+                NewNews.Text = data?.ToString() ?? "No data found";  // Set the TextBlock with the fetched data
+            }
+        }
+
     }
 }
